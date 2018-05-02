aaf3c6e8dcdfbe7331954f5bca1d468d5571b937
root <you@example.com>
2018-05-02 20:09:02 +0800

    IP1

--- update.sh
+++ update.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/dragon/shell/hello-world/ ;
-/usr/bin/dig +short myip.opendns.com @resolver1.opendns.com > README.md && /usr/bin/git add --all && /usr/bin/git commit -am "IP" && /usr/bin/git push origin master && notify-send "已运行"
+cd /root/hello-world/ && /usr/bin/dig +short myip.opendns.com @resolver1.opendns.com > README.md && /usr/bin/git add --all && echo $? && /usr/bin/git commit -am "IP1" && echo $? && /usr/bin/git push origin master && notify-send "已运行"
