51fbc5fb3ef8e9e02b2e8536efcbd4a77efaba29
dragon@ubuntu14.04_newland_i3_1T <1018573693@qq.com>
2018-05-02 17:07:16 +0800

    IP

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/dragon/shell/hello-world/ ;
 dig +short myip.opendns.com @resolver1.opendns.com > README.md; 
-/usr/bin/git commit -a -m "IP" && \
+/usr/bin/git add -A
+/usr/bin/git commit -am "IP" && \
 /usr/bin/git push origin master && \
 notify-send "已运行"
