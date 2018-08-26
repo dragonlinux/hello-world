5ab6dd852ef03c16f73ec4782ccd90cdd002f535
home <you@example.com>
2018-08-27 00:44:02 +0800

    IP5

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /root/hello-world/ && /usr/bin/dig +short myip.opendns.com @resolver1.opendns.com > README.md && /usr/bin/git add --all && echo $? && /usr/bin/git commit -am "IP5" && echo $? && /usr/bin/git push origin master && notify-send "已运行"
+cd /root/hello-world/ && /usr/bin/curl ifconfig.me > README.md && /usr/bin/git add --all && echo $? && /usr/bin/git commit -am "IP5" && echo $? && /usr/bin/git push origin master && notify-send "已运行"
 echo $?
 
