186755c028516831b064f821072ed3e89fa75754
dragon <68>
2020-05-29 00:08:03 +0800

    My IP

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /root/hello-world/ && /usr/bin/curl ifconfig.me > README.md && /usr/bin/git add --all && echo $? && /usr/bin/git commit -am "IP5" && echo $? && /usr/bin/git push origin master && notify-send "已运行"
+cd /root/hello-world/ && /usr/bin/curl ifconfig.me > README.md && /usr/bin/git add --all && echo $? && /usr/bin/git commit -am "My IP" && echo $? && /usr/bin/git push origin master && notify-send "已运行"
 echo $?
 
