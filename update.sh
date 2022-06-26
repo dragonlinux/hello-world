03438556c77e61c86c420f5d308bcaafc6e9d641
2 <dragon@example.com>
2022-06-27 00:25:18 +0800

    IP5

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /root/hello-world/ && /usr/bin/curl ifconfig.me > README.md && /usr/bin/git add --all && echo $? && /usr/bin/git commit -am "IP5" && echo $? && /usr/bin/git push origin master && notify-send "已运行"
+cd /home/dragon/hello-world/ && /usr/bin/curl ifconfig.me > README.md && /usr/bin/git add --all && echo $? && /usr/bin/git commit -am "IP5" && echo $? && /usr/bin/git push origin master && notify-send "已运行"
 echo $?
 
