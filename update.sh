956ed514eaa9944972450d6a67e8fdb5893c4a6a
2 <dragon@example.com>
2022-06-27 00:55:37 +0800

    IP5

--- update.sh
+++ update.sh
@@ -2,3 +2,5 @@
 cd /home/dragon/hello-world/ && /usr/bin/curl ifconfig.me > README.md && /usr/bin/git add --all && echo $? && /usr/bin/git commit -am "IP5" && echo $? && /usr/bin/git push origin master && notify-send "已运行"
 echo $?
 
+echo `date "+%F-%H-%M-%S"` >> log.txt
+
